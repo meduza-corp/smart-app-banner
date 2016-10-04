f565dae19878784da5a6d23a1614127751623656
Anton Byrna <anton.byrna@gmail.com>
2016-10-04 18:15:06 +0400

    Use standalone prop

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ SmartBanner.prototype = {
 
 	create: function() {
 		var link = this.options.link[this.type];
-		var inStore = this.options.price[this.type] + ' - ' + this.options.store[this.type];
+		var inStore = this.options.price[this.type] + ' — ' + this.options.store[this.type];
 		var icon;
 
 		if (this.options.icon) {
